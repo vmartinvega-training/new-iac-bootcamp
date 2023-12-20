758510c6bbf6310739c4598f926b134ec945e404
Your Name <you@example.com>
2023-12-20 14:45:37 +0100

    updated deploy jenkins

---
+++ remove.sh
@@ -0,0 +1,10 @@
+kubectl delete secret jenkins-secrets
+kubectl delete configmap jenkins-conf
+kubectl delete configmap jenkins-git-config
+kubectl delete deployment jenkins
+kubectl delete service jenkins
+kubectl delete service 
+kubectl delete service jenkins-jnlp-port
+kubectl delete service jenkins-tcp-port
+kubectl delete service jenkins-webservice
+kubectl delete pvc jenkins-localhost-pvc
